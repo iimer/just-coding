df4b05a53f01976d47b168fb334f2fa8f06d46d1
cryon <qin.guangyan@sunlord.com.cn>
2023-02-28 15:49:28 +0800

    2023.2.28 daily Q.

---
+++ algorithm/src/com/cryon/hash/base/合并相似的物品2263.java
@@ -0,0 +1,28 @@
+package com.cryon.hash.base;
+
+import java.util.*;
+
+/**
+ * @author iimer
+ * @date 2023-02-28 14:52:03
+ */
+public class 合并相似的物品2263 {
+    public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
+        HashMap<Integer,Integer> v2wCache = new HashMap<>();
+        for (int[] item : items1) {
+            v2wCache.put(item[0],item[1]+v2wCache.getOrDefault(item[0],0));
+        }
+        for (int[] item : items2) {
+            v2wCache.put(item[0],item[1]+v2wCache.getOrDefault(item[0],0));
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> e : v2wCache.entrySet()) {
+            List<Integer> tmpList = new ArrayList<>(2);
+            res.add(tmpList);
+            tmpList.add(e.getKey());
+            tmpList.add(e.getValue());
+        }
+        res.sort(Comparator.comparingInt(l->l.get(0)));
+        return res;
+    }
+}
